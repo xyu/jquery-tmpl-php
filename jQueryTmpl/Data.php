c336797fc9e607a4d0b409e801f1b08af31d6aa3
Xiao Yu <xiao@yu.name>
2010-10-20 21:24:32 -0400

    Created data object to handle test cases

--- jQueryTmpl/Data.php
+++ jQueryTmpl/Data.php
@@ -13,6 +13,15 @@ class jQueryTmpl_Data
     public function getValueOf($jsNotation)
     {
         $dataTokens = $this->_parseJsNotation($jsNotation);
+
+        $localData = $this->_data;
+
+        foreach ($dataTokens as $token)
+        {
+            $localData = $this->_getDataPart($localData, $token);
+        }
+
+        return $localData;
     }
 
     private function _parseJsNotation($js)
@@ -21,7 +30,6 @@ class jQueryTmpl_Data
 
         // Split based on '.' to traverse into object. Could be smarter...
         $loc = explode('.', $js);
-        $len = count($loc);
 
         foreach($loc as $idx => $name)
         {
@@ -32,21 +40,93 @@ class jQueryTmpl_Data
                 throw new jQueryTmpl_Data_Exception('jQueryTmpl_Data can not evaluate expressions.');
             }
 
-            $dataToken = array
-            (
-                'name' => $match[1]
-            );
+            $dataTokens[] = $match[1];
 
             if (!empty($match[2]))
             {
-                // Use trim to extract the core value between brackets
-                $dataToken['subpart'] = trim($match[2], ' \'"[]');
-            }
+                $subparts = explode('][', $match[2]);
 
-            $dataTokens[] = $dataToken;
+                foreach ($subparts as $subname)
+                {
+                    $dataTokens[] = trim($subname, ' \'"[]');
+                }
+            }
         }
 
         return $dataTokens;
+    }
+
+    private function _getDataPart($data, $token)
+    {
+        if ($data instanceof stdClass)
+        {
+            return $this->_getDataPartObject($data, $token);
+        }
+
+        if (is_array($data))
+        {
+            return $this->_getDataPartArray($data, $token);
+        }
+
+        if (is_string($data))
+        {
+            return $this->_getDataPartString($data, $token);
+        }
+
+        // At this point we are told to go into a primitive type, this is undefined
+        return '';
+    }
+
+    private function _getDataPartObject(stdClass $data, $token)
+    {
+        if ($token == 'length')
+        {
+            $i = 0;
+
+            foreach ($data as $element)
+            {
+                $i++;
+            }
+
+            return $i;
+        }
+
+        return $data->$token;
+    }
+
+    private function _getDataPartArray(array $data, $token)
+    {
+        if ($token == 'length')
+        {
+            return count($data);
+        }
+
+        if (!$this->_isInt($token))
+        {
+            return '';
+        }
+
+        return $data[$token];
+    }
+
+    private function _getDataPartString($data, $token)
+    {
+        if ($token == 'length')
+        {
+            return strlen($data);
+        }
+
+        if (!$this->_isInt($token))
+        {
+            return '';
+        }
+
+        return $data[$token];
+    }
+
+    private function _isInt($var)
+    {
+        return ((int)$var == $var);
     }
 }
 
