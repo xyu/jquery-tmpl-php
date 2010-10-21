a6f0f0c3a5d29d4402905303d1be47bd95153794
Xiao Yu <xiao@yu.name>
2010-10-20 20:42:47 -0400

    Adding private method to parse js notation into tokens to be acted upon

--- jQueryTmpl/Data.php
+++ jQueryTmpl/Data.php
@@ -12,6 +12,41 @@ class jQueryTmpl_Data
 
     public function getValueOf($jsNotation)
     {
+        $dataTokens = $this->_parseJsNotation($jsNotation);
+    }
+
+    private function _parseJsNotation($js)
+    {
+        $dataTokens = array();
+
+        // Split based on '.' to traverse into object. Could be smarter...
+        $loc = explode('.', $js);
+        $len = count($loc);
+
+        foreach($loc as $idx => $name)
+        {
+            $match = array();
+
+            if (!preg_match('/^([a-z_$][0-9a-z_$]*)(\[.*\])?$/i', $name, $match))
+            {
+                throw new jQueryTmpl_Data_Exception('jQueryTmpl_Data can not evaluate expressions.');
+            }
+
+            $dataToken = array
+            (
+                'name' => $match[1]
+            );
+
+            if (!empty($match[2]))
+            {
+                // Use trim to extract the core value between brackets
+                $dataToken['subpart'] = trim($match[2], ' \'"[]');
+            }
+
+            $dataTokens[] = $dataToken;
+        }
+
+        return $dataTokens;
     }
 }
 
